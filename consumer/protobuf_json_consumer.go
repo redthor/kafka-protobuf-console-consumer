fc4eeb0a26c88793ec90ad966394fa28895c0675
Mikhalev Ruslan <DNAlchemist@ya.ru>
2020-03-05 10:23:37 +0000

    Fix build

--- consumer/protobuf_json_consumer.go
+++ consumer/protobuf_json_consumer.go
@@ -22,7 +22,7 @@ func (SimpleConsumerGroupHandler) Setup(_ sarama.ConsumerGroupSession) error   {
 func (SimpleConsumerGroupHandler) Cleanup(_ sarama.ConsumerGroupSession) error { return nil }
 func (h SimpleConsumerGroupHandler) ConsumeClaim(sess sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
 	for msg := range claim.Messages() {
-		if messageInfo {
+		if h.messageInfo {
 			fmt.Printf("Message key:%q topic:%q partition:%d offset:%d\n", msg.Key, msg.Topic, msg.Partition, msg.Offset)
 		}
 		jsonString, e := h.protobufJSONStringify.JsonString(msg.Value, h.prettyJson)
